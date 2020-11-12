28b6d3d83aa4560148f966afcc38b64fbdb4ada9
cgesacion <cameron.gesacion@patelhs.org>
2020-11-12 18:55:08 +0000

    I imputed an array of 15 elements

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+ public static void main(String[] args){
+    int[] z = new int[10];
+    z[0] = 223;
+    z[1] = 23;
+    int[] x = {23,32,43,12,43,45,69,84,26,77,59,21,13,53,68};
+    System.out.println(z[0]);
+    System.out.println(z[1]);
+    System.out.println(x[3]);
+    System.out.println(x[4]);
+    System.out.println(x[5]);
+    System.out.println(x[6]);
+    System.out.println(x[7]);
+    System.out.println(x[8]);
+    System.out.println(x[9]);
+    System.out.println(x[10]);
+    System.out.println(x[11]);
+    System.out.println(x[12]);
+    System.out.println(x[13]);
+    System.out.println(x[14]);
   }
-}
+} 
